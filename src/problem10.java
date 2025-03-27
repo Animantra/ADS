2cefdb11aed1c5b618b090f678c2320605bdd480
Animantra <t20227893@gmail.com>
2025-03-27 09:04:40 +0500

    Commit message

--- src/problem10.java
+++ src/problem10.java
@@ -8,9 +8,13 @@ public class problem10 {
         System.out.println(gcd(a,b));
     }
     public static int gcd(int a, int b){
+        if(a==0){
+            return b;
+        }
         if(b==0){
             return a;
         }
+
         return gcd(b,a%b);
     }
 }
